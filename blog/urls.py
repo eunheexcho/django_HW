2bb11d0425501d8f764df113a67ecf1dbca78d76
eunheexcho <vialactea128@gmail.com>
2019-01-26 17:00:42 +0900

    example of url reverse

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^$', views.post_list),
-    re_path(r'^(?P<id>\d+)/$', views.post_detail),
+    re_path(r'^$', views.post_list, name='post_list'),
+    re_path(r'^(?P<id>\d+)/$', views.post_detail, name='post_detail'),
 ]
