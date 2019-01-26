2a1f74b890b3ffc629dd5a445b56d65b0e38dd2a
eunheexcho <vialactea128@gmail.com>
2019-01-26 18:29:15 +0900

    blog/views_cbv

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, re_path
 from . import views
+from . import views_cbv
 
 app_name = 'blog'
 urlpatterns = [
     re_path(r'^$', views.post_list, name='post_list'),
     re_path(r'^(?P<id>\d+)/$', views.post_detail, name='post_detail'),
+    path('cbv/news/', views_cbv.post_new),
 ]
