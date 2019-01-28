db23d48dc9acaf78f761bc42124c60124e2326bd
eunheexcho <vialactea128@gmail.com>
2019-01-29 03:02:45 +0900

    dojo_revised

--- dojo/urls.py
+++ dojo/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from django.urls import include, path
-from .import views
+from django.urls import include, path, re_path
+from . import views
 
 app_name = 'dojo'
 urlpatterns = [
-    path('views/', views.mysum),
+    re_path(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
+    re_path(r'^hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)/$', views.hello),
+    re_path(r'^list1/$', views.post_list1),
+    re_path(r'^list2/$', views.post_list2),
+    re_path(r'^list3/$', views.post_list3),
+    re_path(r'^excel/$', views.excel_download),
 ]
