936967ef6f8a6c88d22fc416fb7066f9ea37e294
eunheexcho <vialactea128@gmail.com>
2019-02-01 16:00:53 +0900

    media file serving

--- first/urls.py
+++ first/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.shortcuts import redirect
@@ -30,6 +31,8 @@ urlpatterns = [
     path('dojo/', include('dojo.urls', namespace='dojo')),
     path('shop/', include('shop.urls', namespace='shop')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
