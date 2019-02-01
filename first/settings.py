1e0873a17aa960c138156432f2f9c5e1cd7f4dd4
eunheexcho <vialactea128@gmail.com>
2019-02-01 14:49:44 +0900

    static (not working well)

--- first/settings.py
+++ first/settings.py
@@ -127,6 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'first', 'static'),
+    os.path.join(BASE_DIR, 'blog', 'static'),
+]
+
 
 INTERNAL_IPS = ['127.0.0.1']
 
