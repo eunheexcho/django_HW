12b6c4cd261c9dcbb73420d378974e5fdd68393c
eunheexcho <vialactea128@gmail.com>
2019-02-07 19:08:01 +0900

    DetailView

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ from . import views_cbv
 app_name = 'blog'
 urlpatterns = [
     re_path(r'^$', views_cbv.post_list, name='post_list'),
-    re_path(r'^(?P<id>\d+)/$', views.post_detail, name='post_detail'),
+    re_path(r'^(?P<pk>\d+)/$', views_cbv.post_detail, name='post_detail'),
     re_path(r'^new/$', views.post_new, name='post_new'),
     re_path(r'^(?P<id>\d+)/edit/$', views.post_edit, name='post_edit'),
     path('cbv/new/', views_cbv.post_new),
