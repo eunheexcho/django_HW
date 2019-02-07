39cac3772062c9b16898f3fd9cc371239861a0b7
eunheexcho <vialactea128@gmail.com>
2019-02-07 18:34:45 +0900

    dojo CBV sample step 4-1

--- dojo/urls.py
+++ dojo/urls.py
@@ -5,6 +5,7 @@ from . import views, views_cbv
 app_name = 'dojo'
 urlpatterns = [
     re_path(r'^new/$', views.post_new),
+    re_path(r'^(?P<pk>\d+)/$', views.post_detail),
     re_path(r'^(?P<id>\d+)/edit/$', views.post_edit),
     re_path(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
     re_path(r'^hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)/$', views.hello),
