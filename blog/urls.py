025830130f50df5f3d05a9a5b5f94d2f4f4aaae2
eunheexcho <vialactea128@gmail.com>
2019-02-07 19:48:36 +0900

    DeleteView

--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     re_path(r'^new/$', views.post_new, name='post_new'),
     re_path(r'^(?P<id>\d+)/edit/$', views.post_edit, name='post_edit'),
     path('cbv/new/', views_cbv.post_new),
-    re_path(r'^cbv/(?P<pk>\d+)/edit/$', views_cbv.post_edit)
+    re_path(r'^cbv/(?P<pk>\d+)/edit/$', views_cbv.post_edit),
+    re_path(r'^cbv/(?P<pk>\d+)/delete/$', views_cbv.post_delete),
 ]
