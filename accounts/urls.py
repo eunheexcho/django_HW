e320f2c517edf40a214b26016a908d5eab32869c
eunheexcho <vialactea128@gmail.com>
2019-02-07 23:58:16 +0900

    Login quiz

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, re_path
 from django.contrib.auth import views as auth_views
 from django.conf import settings
+from .forms import LoginForm
 from . import views
+
 
 urlpatterns = [
     re_path(r'^signup/$', views.signup, name='signup'),
-    re_path(r'^login/$', auth_views.login, name='login', kwargs={'template_name': 'accounts/login_form.html'}),
+    re_path(r'^login/$', auth_views.login, name='login',
+            kwargs={
+                'authentication_form': LoginForm,
+                'template_name': 'accounts/login_form.html'
+            }),
     re_path(r'^logout/$', auth_views.logout, name='logout', kwargs={'next_page': settings.LOGIN_URL}),
     re_path(r'^profile/$', views.profile, name='profile'),
 ]
